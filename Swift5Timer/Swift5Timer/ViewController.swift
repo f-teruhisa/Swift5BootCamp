93a4071dd5d221f3239475d5f514d4e5a18c7441
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-02-29 16:54:10 +0900

    add Swift5Timer project

---
+++ Swift5Timer/Swift5Timer/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Swift5Timer
+//
+//  Created by MBP010 on 2020/02/29.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageVIew: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func start(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stop(_ sender: Any) {
+    }
+    
+}
+
