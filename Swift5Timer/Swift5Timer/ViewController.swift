2ab36abbeb2dba5fc2d5fd0bef45e877bf1c1b34
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-01 14:13:14 +0900

    add imageArray

--- Swift5Timer/Swift5Timer/ViewController.swift
+++ Swift5Timer/Swift5Timer/ViewController.swift
@@ -14,11 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
+    var timer = Timer()
+    var count = Int()
+    var imageArray = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        count = 0
         // stopButtonを押せなくする
         stopButton.isEnabled = true
+        
+        // Property
+    }
+    
+    func startTimer(){
+        // タイマーを回す(0.2秒ごと)
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func timerUpdate(){
+      // 0.2秒ごとに呼ばれる
+        count = count + 1
+        imageView.image = imageArray[count]
     }
     
     
