a6c3d44c7acdfbd080df1052dfe92d209fd41340
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-01 19:21:33 +0900

    initialized CalcApp

---
+++ CalcApp/CalcApp/Model/Car.swift
@@ -0,0 +1,27 @@
+//
+//  Car.swift
+//  CalcApp
+//
+//  Created by MBP010 on 2020/03/01.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import Foundation
+
+class Car{
+    
+    var frontWheel = 0
+    var rearWheel = 0
+    
+    // initialize = 初期化
+    init(){
+        
+        frontWheel = 2
+        rearWheel = 2
+        
+    }
+    
+    func drive(){
+        
+    }
+}
---
+++ CalcApp/CalcApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by MBP010 on 2020/03/01.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func doAction(_ sender: Any) {
+    }
+    
+}
+
