ab8ded8765cc466d91f8502ab4ea1cf1d50851ce
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-07 15:21:35 +0900

    initialize Swift5senni

---
+++ Siwft5Senni/Siwft5Senni/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Siwft5Senni
+//
+//  Created by MBP010 on 2020/03/07.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
