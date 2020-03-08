bf99297449595051fbe061e85aab238abf76e32b
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-08 22:14:30 +0900

    definate webView size

--- Swift5WKWebView1/Swift5WKWebView1/ViewController.swift
+++ Swift5WKWebView1/Swift5WKWebView1/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    var webView = WKWebView()
 
+    @IBOutlet weak var toolBar: UIToolbar!
+    
     @IBOutlet weak var indicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 実態になっている
+        
+        // 大きさ
+        webView.frame = CGRect(x: 0, y: toolBar.frame.size.height, width: view.frame.size.width, height: view.frame.size.height - toolBar.frame.size.height*2)
+        view.addSubview(webView)
+        
+        // 何をロードするのか
+        
+        // どこにつけるのか
+        
+        
     }
 
     @IBAction func go(_ sender: Any) {
