adca140affda5a7dc930752fb7aea4d5773bbb97
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-08 14:41:17 +0900

    initialize WVKit

---
+++ Swift5WKWebView1/Swift5WKWebView1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift5WKWebView1
+//
+//  Created by MBP010 on 2020/03/08.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func go(_ sender: Any) {
+    }
+    
+    @IBAction func back(_ sender: Any) {
+    }
+    
+}
+
