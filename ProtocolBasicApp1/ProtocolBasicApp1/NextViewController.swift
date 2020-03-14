cdad2887b719c59548f09ed802e1fba8b165814c
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-14 23:22:30 +0900

    add NextViewController

--- ProtocolBasicApp1/ProtocolBasicApp1/NextViewController.swift
+++ ProtocolBasicApp1/ProtocolBasicApp1/NextViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var count:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +21,10 @@ class NextViewController: UIViewController {
     }
     
 
+    @IBAction func plusAction(_ sender: Any) {
+        count = count + 1
+        label.text = String(count)
+    }
     /*
     // MARK: - Navigation
 
