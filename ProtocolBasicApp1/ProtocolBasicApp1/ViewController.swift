532dfc52a211204e73a62f2e0233d7ede25ecb3b
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-14 23:47:07 +0900

    deligate is enabled in UIViewController

--- ProtocolBasicApp1/ProtocolBasicApp1/ViewController.swift
+++ ProtocolBasicApp1/ProtocolBasicApp1/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// プロトコルを宣言する
+class ViewController: UIViewController, CatchProtocol {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var vount = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    func catchData(count: Int) {
+        label.text = String(count)
     }
 
-
+    @IBAction func next(_ sender: Any) {
+        performSegue(withIdentifier: "next", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! NextViewController
+        nextVC.deligate = self
+    }
 }
 
