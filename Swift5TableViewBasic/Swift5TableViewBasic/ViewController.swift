e7f5d90e4f12d7e762bbbf9c6069ce547c357728
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-14 13:45:50 +0900

    initialize Swift5TableVIew

---
+++ Swift5TableViewBasic/Swift5TableViewBasic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5TableViewBasic
+//
+//  Created by MBP010 on 2020/03/14.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
