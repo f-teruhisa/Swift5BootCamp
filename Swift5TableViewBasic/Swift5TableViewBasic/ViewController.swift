5215aeab51693ad7ab06984fb696b65fc4591cb8
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-14 15:07:32 +0900

    defination table cell and rows´with textArray

--- Swift5TableViewBasic/Swift5TableViewBasic/ViewController.swift
+++ Swift5TableViewBasic/Swift5TableViewBasic/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var textArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +27,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return textArray.count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -33,15 +35,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = textArray[indexPath.row]
+        cell.imageView!.image = UIImage(named: "checkImage")
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return view.frame.size.height/6
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textArray.append(textField.text!)
+        textField.resignFirstResponder()
+        textField.text = ""
+        tableView.reloadData()
+        
+        return true
     }
 
 
