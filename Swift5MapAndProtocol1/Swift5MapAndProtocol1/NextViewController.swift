9e31ee6689c560284d43b606db2942e2a8d2fa5a
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-15 18:59:10 +0900

    implementate search locationprotocol

--- Swift5MapAndProtocol1/Swift5MapAndProtocol1/NextViewController.swift
+++ Swift5MapAndProtocol1/Swift5MapAndProtocol1/NextViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
+protocol SearchLocationDeligate {
+    func searchLocation(idoValue:String, keidoValue:String)
+}
+
 class NextViewController: UIViewController {
     
     @IBOutlet weak var idoTextField: UITextField!
     @IBOutlet weak var keidoTextField: UITextField!
+    
+    var deligate:SearchLocationDeligate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func okAction(_ sender: Any) {
+        // Get texts in textFields
+        let idoValue = idoTextField.text!
+        let keidoValue = keidoTextField.text!
+                
+        // set texts into args in deligate method
+        deligate?.searchLocation(idoValue: idoValue, keidoValue: keidoValue)
+        
+        // dismiss unless two textFIelds are not empty
+        if idoTextField.text != nil && keidoTextField.text != nil{
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
     
 
     /*
