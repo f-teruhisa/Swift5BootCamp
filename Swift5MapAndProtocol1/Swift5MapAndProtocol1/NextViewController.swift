fb380a9e1acbb476778263ebd3b16426378b8709
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-15 14:14:35 +0900

    initialized map app´

---
+++ Swift5MapAndProtocol1/Swift5MapAndProtocol1/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  Swift5MapAndProtocol1
+//
+//  Created by MBP010 on 2020/03/15.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
