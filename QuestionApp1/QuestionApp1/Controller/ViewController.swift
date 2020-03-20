4dc69980d2a43472027a12b06f4a6aadd6b1dcca
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-20 14:45:15 +0900

    implimentation image counter

--- QuestionApp1/QuestionApp1/Controller/ViewController.swift
+++ QuestionApp1/QuestionApp1/Controller/ViewController.swift
@@ -10,23 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var maxScoreLabel: UILabel!
+    
+    var correctCount = 0
+    var wrongCount = 0
+    var maxScore = 0
+    var questionNumber = 0
+    
+    let imagesList = ImagesList()
+    
+    // IBActionで検知した正答がどちらなのかを取得する変数
+    var pickedAnswer = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        correctCount = 0
+        wrongCount = 0
+        questionNumber = 0
+        
+        imageView.image = UIImage(named: imagesList.list[0].imageText)
+    }
 
     @IBAction func answer(_ sender: Any) {
         if (sender as AnyObject).tag == 1{
             // ○ボタンが押されたとき
-            
             // ユーザーが押したボタンが○ボタンだった
+            pickedAnswer = true
             
             // ○ボタンの音声を流す
             
         }else if (sender as AnyObject).tag == 2{
+            pickedAnswer = false
             // ✗ボタンが押されたとき
             
             // ユーザーが押したボタンが✗ボタンだった
@@ -34,6 +54,13 @@ class ViewController: UIViewController {
             // ✗ボタンの音声を流す
             
         }
+        
+        // 回答があっているかどうかをチェックする
+        check()
+    }
+    
+    func check(){
+        
     }
     
 }
