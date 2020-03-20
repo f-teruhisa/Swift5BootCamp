e701641d7119201c9a73dbcf284d0061361cf07c
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-20 13:10:52 +0900

    initialize question app

---
+++ QuestionApp1/QuestionApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionApp1
+//
+//  Created by MBP010 on 2020/03/20.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
