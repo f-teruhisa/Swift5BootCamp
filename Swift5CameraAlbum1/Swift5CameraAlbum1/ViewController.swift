0fe4197b05502df3ce47ac734e3c212a00687253
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-21 18:17:16 +0900

    initialized camera app

---
+++ Swift5CameraAlbum1/Swift5CameraAlbum1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5CameraAlbum1
+//
+//  Created by MBP010 on 2020/03/21.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
