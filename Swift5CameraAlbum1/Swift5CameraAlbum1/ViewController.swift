f04ce96ed7e088feac47fea5c821854856597bf3
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-21 18:26:20 +0900

    add PHPhoto requirement

--- Swift5CameraAlbum1/Swift5CameraAlbum1/ViewController.swift
+++ Swift5CameraAlbum1/Swift5CameraAlbum1/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var backImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        PHPhotoLibrary.requestAuthorization{ (status) in
+            switch(status) {
+            case .authorized:
+                print("許可されてます")
+            case .denied:
+                print("拒否されました")
+            case .notDetermined:
+                print("not determined")
+            case .restricted:
+                print("restrivted")
+            }
+        }
     }
 
 
