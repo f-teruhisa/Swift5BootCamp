9f9b122496a54674e9ef9f559e28f9d8340ae084
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-22 20:36:12 +0900

    implementation getImage function

--- Swift5Bokete1/Swift5Bokete1/ViewController.swift
+++ Swift5Bokete1/Swift5Bokete1/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var commentTextView: UITextView!
     @IBOutlet weak var searchTextField: UITextField!
     
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,17 +28,45 @@ class ViewController: UIViewController {
         
         PHPhotoLibrary.requestAuthorization{ (status) in
             switch(status){
-            case .authorized: break
-            case .denied: break
-            case .notDetermined: break
-            case .restricted: break
+                case .authorized: break
+                case .denied: break
+                case .notDetermined: break
+                case .restricted: break
             }
         }
+        getImages(keyword: "funny")
     }
     
     // 検索キーワードの値を元に画像を引っ張ってくる
     // pixabay.com
+    func getImages(keyword:String){
+        // APIキー
+        let url = "https://pixabay.com/api/?key=15691457-7ca44a1b818ed81f202e6c789&q=\(keyword)"
+        
+        // Alamofireを使って、HTTPリクエストを投げる
+        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON{ (response) in
+            switch response.result{
+                case .success:
+                    let json:JSON = JSON(response.data as Any)
+                    let imageString = json["hits"][self.count]["webformatURL"].string
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                
+                case .failure(let error):
+                    print(error)
+            }
+        }
+        
+        // レスポンスをJSON解析する
+        
+        // imageView.imageに貼り付ける
+        
+    }
 
-
+    
+    @IBAction func nextOdai(_ sender: Any) {
+        count = count + 1
+        // getImages(keyword: <#T##String#>)
+    }
+    
 }
 
