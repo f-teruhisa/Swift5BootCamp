f1cf1cc18ba10c832ab4cd8cbaab5e7a405bc942
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-22 21:56:01 +0900

    complete bokete app

--- Swift5Bokete1/Swift5Bokete1/ShareViewController.swift
+++ Swift5Bokete1/Swift5Bokete1/ShareViewController.swift
@@ -22,9 +22,42 @@ class ShareViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        resultImageView.image = resultImage
+        commentLabel.text = commentString
+        commentLabel.adjustsFontSizeToFitWidth = true
     }
     
+    
+    
 
+    @IBAction func share(_ sender: Any) {
+        //スクリーンショットを撮る
+        takeScreenShot()
+        
+        let items = [screenShotImage] as [Any]
+        // アクティビティビューに乗せ、シェアする
+        let activityVC = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        
+        present(activityVC, animated: true, completion: nil)
+    }
+    
+    func takeScreenShot(){
+        let width = CGFloat(UIScreen.main.bounds.size.width)
+        let height = CGFloat(UIScreen.main.bounds.size.height/1.3)
+        let size = CGSize(width: width, height: height)
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        
+        // viewに書き出す
+        self.view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        screenShotImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
