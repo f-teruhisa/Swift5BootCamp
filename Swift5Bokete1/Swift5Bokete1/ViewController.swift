a1faec60b55dcbc959f55b53f7c0288394b0812d
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-03-22 15:57:56 +0900

    implementation basic UI View controller

--- Swift5Bokete1/Swift5Bokete1/ViewController.swift
+++ Swift5Bokete1/Swift5Bokete1/ViewController.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+import Photos
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var odaiImageView: UIImageView!
+    @IBOutlet weak var commentTextView: UITextView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        commentTextView.layer.cornerRadius = 20.0
+        
+        PHPhotoLibrary.requestAuthorization{ (status) in
+            switch(status){
+            case .authorized: break
+            case .denied: break
+            case .notDetermined: break
+            case .restricted: break
+            }
+        }
     }
+    
+    // 検索キーワードの値を元に画像を引っ張ってくる
+    // pixabay.com
 
 
 }
