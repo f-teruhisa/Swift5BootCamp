4f0f59252d943ff6bbd530bc7a6d8ae5245bf7f6
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-06-28 20:29:48 +0900

    init Swift5FacebookLoginWithFirebase1

---
+++ Swift5FacebookLoginWithFirebase1/Swift5FacebookLoginWithFirebase1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5FacebookLoginWithFirebase1
+//
+//  Created by MBP010 on 2020/06/28.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
