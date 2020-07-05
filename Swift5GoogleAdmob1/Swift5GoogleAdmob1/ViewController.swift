5136a278fdfbadcb4eebaba28fc52f22a46d0479
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-07-05 19:56:13 +0900

    initialized google admob

---
+++ Swift5GoogleAdmob1/Swift5GoogleAdmob1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5GoogleAdmob1
+//
+//  Created by MBP010 on 2020/07/05.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
