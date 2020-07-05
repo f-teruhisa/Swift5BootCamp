d361c9e6f085f890cb8efe4391b93cb5c5e0d32a
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-07-05 21:31:11 +0900

    complete return ad cells

--- Swift5GoogleAdmob1/Swift5GoogleAdmob1/TableViewController.swift
+++ Swift5GoogleAdmob1/Swift5GoogleAdmob1/TableViewController.swift
@@ -9,83 +9,84 @@
 import UIKit
 import GoogleMobileAds
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, GADBannerViewDelegate, GADInterstitialDelegate {
+    
+    var profileImageArray = ["0", "1", "2", "3", "4"]
+    var textArray = ["顔文字0", "顔文字1", "顔文字2", "顔文字3", "インタースティシャル広告"]
+    var interstitial:GADInterstitial!
+    
+    let backImageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let image = UIImage(named: "backGrondImage")
+        backImageView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        backImageView.image = image
+        tableView.backgroundView = backImageView
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1 + textArray.count
     }
 
-    /*
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let indexNubmer = indexPath.row
+        
+        if indexPath.row == 0{
+            // インタースティシャル広告のセル
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BannerCell", for: indexPath)
+            let bannerView = cell.viewWithTag(1) as! GADBannerView
+            bannerView.adUnitID = "ca-app-pub-3940256099942544~1458002511"
+            bannerView.rootViewController = self
+            bannerView.load(GADRequest())
+            
+            return cell
+        }else{
+            let cell2 = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            
+            let profileImageView = cell2.viewWithTag(1) as! UIImageView
+            profileImageView.image = UIImage(named: profileImageArray[indexNubmer - 1])
+            let nameLabel = cell2.viewWithTag(2) as! UILabel
+            nameLabel.textColor = .white
+            nameLabel.font = .boldSystemFont(ofSize: 20)
+            nameLabel.numberOfLines = 3
+            nameLabel.text = textArray[indexNubmer - 1]
+            
+            return cell2
+            
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 5{
+            if interstitial.isReady{
+                interstitial.present(fromRootViewController: self)
+            }else{
+                print("まだ広告の準備ができておりません")
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func createAndLoadInterstitial() -> GADInterstitial{
+        var interstitial = GADInterstitial(adUnitID: "")
+        interstitial.delegate = self
+        interstitial.load(GADRequest)
+        
+        return interstitial
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
