090ff5d52c5b6b34e5547736097cc39d400c5694
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-07-07 22:50:17 +0900

    xib table cell

---
+++ ChatApp1/ChatApp1/View/CustomCellTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomCellTableViewCell.swift
+//  ChatApp1
+//
+//  Created by MBP010 on 2020/07/07.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
