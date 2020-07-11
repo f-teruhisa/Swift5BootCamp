ba67022427e2776f7b920c8851215fa59f48b8ab
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-07-11 18:25:29 +0900

    add anonymous login with Facebook App

---
+++ Swift5FirebaseAnonymousLogin/Swift5FirebaseAnonymousLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5FirebaseAnonymousLogin
+//
+//  Created by MBP010 on 2020/07/11.
+//  Copyright © 2020 Teruhisa Fukumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
